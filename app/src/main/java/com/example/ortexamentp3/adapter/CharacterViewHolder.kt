5be06586f536a4fcc16d1cbd559ffaadeebc5f01
Federico Vazquez <federicoarielvazquez@gmail.com>
2022-11-04 15:51:21 -0300

    recyclerView con material card y glide, mejoras esteticas, fragment con detalle de personaje tambien

--- app/src/main/java/com/example/ortexamentp3/adapter/CharacterViewHolder.kt
+++ app/src/main/java/com/example/ortexamentp3/adapter/CharacterViewHolder.kt
@@ -1,16 +1,24 @@
 package com.example.ortexamentp3.adapter
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.ortexamentp3.R
 import com.example.ortexamentp3.domain.viewModel.Character
 
 class CharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val titleText: TextView = itemView.findViewById(R.id.item_title)
+    private val characterName:    TextView = itemView.findViewById(R.id.textViewName)
+    private val characterStatus : TextView = itemView.findViewById(R.id.textViewStatus)
+    private val characterImage  : ImageView = itemView.findViewById(R.id.character_image)
 
     fun bind(character : Character) {
-        titleText.text = character.name
+        characterName.text = character.name
+        characterStatus.text = "Status:" + character.status // ojo debe ser Status
+        Glide.with(itemView)
+            .load(character.image)
+            .into(characterImage)
     }
 
 }
--- app/src/main/java/com/example/ortexamentp3/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/ortexamentp3/fragments/DetailFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.example.ortexamentp3.R
 
 class DetailFragment : Fragment() {
@@ -27,14 +29,27 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textViewName    = view.findViewById<TextView>(R.id.textViewName)
-        val textViewOrigin  = view.findViewById<TextView>(R.id.textViewOriginName)
-        val buttonBack      = view.findViewById<Button>(R.id.buttonBack)
 
-        textViewName.text   = DetailFragmentArgs.fromBundle(requireArguments()).character.name
-        textViewOrigin.text = DetailFragmentArgs.fromBundle(requireArguments()).character.origin.name
 
-          buttonBack.setOnClickListener {
+        val textViewStatus  = view.findViewById<TextView>(R.id.textViewDetailStatus)
+        val textViewName    = view.findViewById<TextView>(R.id.textViewDetailName)
+        val textViewSpecies = view.findViewById<TextView>(R.id.textViewDetailSpecies)
+        val textViewOrigin  = view.findViewById<TextView>(R.id.textViewDetailOrigin)
+        val buttonAddToFav  = view.findViewById<Button>(R.id.buttonAddToFavorites)
+
+        textViewStatus.text  = DetailFragmentArgs.fromBundle(requireArguments()).character.status
+        textViewName.text    = DetailFragmentArgs.fromBundle(requireArguments()).character.name
+        textViewSpecies.text = "Especie: " + DetailFragmentArgs.fromBundle(requireArguments()).character.species
+        textViewOrigin.text  = "Origen: "  + DetailFragmentArgs.fromBundle(requireArguments()).character.origin.name
+
+        val imgUrl = DetailFragmentArgs.fromBundle(requireArguments()).character.image
+        val characterImage = view.findViewById<ImageView>(R.id.characterDetail_image)
+
+        Glide.with(view)
+            .load(imgUrl)
+            .into(characterImage)
+
+          buttonAddToFav.setOnClickListener {
             val actionBack = DetailFragmentDirections.actionDetailFragmentToHomeFragment()
             view.findNavController().navigate((actionBack))
         }
