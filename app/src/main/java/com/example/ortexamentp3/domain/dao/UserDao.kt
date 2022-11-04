c279beb251873e20ec3f1d7daf234feccc34fd11
Franco Redoni <franco.redoni@gmail.com>
2022-11-03 21:45:40 -0300

    Agregado de room (sin conexión con lógica) y pantalla de login. Además arreglo nav component para que arranque por el login

---
+++ app/src/main/java/com/example/ortexamentp3/domain/dao/UserDao.kt
@@ -0,0 +1,26 @@
+package com.example.ortexamentp3.domain.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ortexamentp3.domain.models.User
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<User>
+
+    @Query("SELECT * FROM user WHERE name LIKE :userName AND " +
+            "password LIKE :userPassword LIMIT 1")
+    fun findByName(userName: String, userPassword: String): User
+
+    @Insert(entity = User::class)
+    fun insert(user: User)
+
+    @Delete
+    fun delete(user: User)
+}
---
+++ app/src/main/java/com/example/ortexamentp3/domain/repositories/UserRepository.kt
@@ -0,0 +1,43 @@
+package com.example.ortexamentp3.domain.repositories
+
+import android.content.Context
+import androidx.room.Room
+import com.example.ortexamentp3.domain.dao.UserDao
+import com.example.ortexamentp3.domain.database.AppDatabase
+import com.example.ortexamentp3.domain.models.User
+
+class UserRepository private constructor(private val appDatabase: AppDatabase) {
+
+    private val userDao : UserDao = appDatabase.userDao()
+
+    fun addUser(user: User){
+        userDao.insert(user)
+    }
+
+    fun removeUser(user: User){
+        userDao.delete(user)
+    }
+
+    fun getAllUsers() : List<User>{
+        return userDao.getAll()
+    }
+
+    companion object{
+        private var userRepository: UserRepository? = null
+
+        fun getInstance(context: Context) : UserRepository{
+            return userRepository ?: kotlin.run{
+
+                val db = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "appDatabase"
+                ).build()
+
+                val createdUserRepository = UserRepository(db)
+                userRepository = UserRepository(db)
+                createdUserRepository
+            }
+        }
+    }
+}
