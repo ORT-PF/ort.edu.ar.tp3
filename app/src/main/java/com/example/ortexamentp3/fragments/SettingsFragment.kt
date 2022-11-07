009855d1a6fba86fd51dc4d6e7e44757137c5d8e
Facundo Verbauwede <facundoverbauwede13@gmail.com>
2022-11-07 12:56:41 -0300

    agrego funcionalidad al switch de night mode

--- app/src/main/java/com/example/ortexamentp3/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/ortexamentp3/fragments/SettingsFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import com.example.ortexamentp3.R
 class SettingsFragment : Fragment() {
@@ -14,6 +16,7 @@ class SettingsFragment : Fragment() {
 
     lateinit var view1: View
     lateinit var buttonGoToHome: Button
+    lateinit var nightMode: Switch
 
 
     override fun onCreateView(
@@ -22,7 +25,12 @@ class SettingsFragment : Fragment() {
     ): View? {
         view1 = inflater.inflate(R.layout.fragment_settings, container, false)
 
+
         buttonGoToHome = view1.findViewById(R.id.backButton)
+        nightMode = view1.findViewById(R.id.night_mode)
+
+
+
 
         return view1
     }
@@ -34,6 +42,17 @@ class SettingsFragment : Fragment() {
             view1.findNavController().navigate(R.id.action_settingsFragment2_to_homeFragment)
         }
 
+
+        nightMode.setOnCheckedChangeListener { _, _ ->
+            if (nightMode.isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
     }
+
+
 
 }
